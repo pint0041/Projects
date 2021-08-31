35ef6890ca5f45bb78f4c0bbde5a8e778ec75b88
pint0041 <pint0041@algonquinlive.com>
2021-08-31 13:08:59 -0400

    adding 3 and 4

---
+++ Python/pythonapp003.py
@@ -0,0 +1,12 @@
+# Variables and Data types 15:42 https://www.youtube.com/watch?v=rfscVS0vtbw
+
+character_name = "John"
+character_age = 50
+isMale = False
+
+print("There once was a man named " + character_name + ", " )
+print("he was a " + character_age + " year old")
+
+character_name = "Mike"
+print("He really like his name " + character_name +", ")
+print("But didn't like being " + character_age + ".")
---
+++ Python/pythonapp004.py
@@ -0,0 +1,17 @@
+# Working with strings 27:35 https://www.youtube.com/watch?v=rfscVS0vtbw
+
+phrase =  "Giraffe Academy"
+print("Giraffe\"Academy")
+
+print(phrase.lower())
+print(phrase.upper())
+print(phrase.isupper())
+print(phrase.upper().isupper())
+print(len(phrase))
+
+
+print(phrase[0])
+
+print(phrase.index("G"))
+
+print(phrase.replace("Giraffe", "Elephant"))
